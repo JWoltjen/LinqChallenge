7f5d3412e0a7b720a092ae029d3bc991a2b0d552
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-07-28 11:38:43 -0500

    MVP commit.

--- LinqChallenge/Program.cs
+++ LinqChallenge/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace LinqChallenge
 {
+    
     class Program
     {
+        
         static void Main(string[] args)
         {
-           
+            string words = "Create a LINQ query that takes in a string and returns a list of all the letters in order, regardless of case";
+            List<char> sortedChars = CharSorter(words);
+            foreach(char c in sortedChars)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+        }
+        static List<char> CharSorter(string words)
+        {
+            var sortedWords = words
+                    .ToCharArray()
+                    .Where(char.IsLetter)
+                    .OrderBy(char.ToLower)
+                    .ToList();
+            return sortedWords;
         }
     }
 }
