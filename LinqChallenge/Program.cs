19661618262a9c99616ec0e3ab59af7f81bd35fb
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-07-28 12:12:55 -0500

    Annotate code to explain what reflection is doing

--- LinqChallenge/Program.cs
+++ LinqChallenge/Program.cs
@@ -16,6 +16,18 @@ namespace LinqChallenge
             var indexedChars = CharIndexer(moreWords);
             foreach(var c in indexedChars)
             {
+                // Here, we are using reflection to access the properties of an anonymous type. 
+                // The anonymous type was created by the LINQ query in the CharIndexer method, 
+                // and it has properties named "Letter" and "Count". Because the type is anonymous, 
+                // we don't know its name at compile time, so we can't access its properties directly 
+                // in the usual way (i.e., c.Letter and c.Count). Instead, we use reflection to access 
+                // the properties. We get the Type of the object (c.GetType()), then we get the PropertyInfo 
+                // for each property (GetProperty("Letter") and GetProperty("Count")), and finally we get 
+                // the value of each property for the object (GetValue(c, null)).
+                //
+                // Note that while reflection can be slower and more compute-heavy than direct code operations, 
+                // the performance impact in this specific case is likely to be negligible, because we're only 
+                // using reflection to access a couple of properties of a small number of objects.
                 {
                     var letter = c.GetType().GetProperty("Letter").GetValue(c, null);
                     var count = c.GetType().GetProperty("Count").GetValue(c, null);
